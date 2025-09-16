c1f198cd493b80b2799998d07f476c80758465d5
testlearnernord <toleora@gmail.com>
2025-09-17 01:19:21 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 
+// IMPORTANT for GitHub Pages: base must match the repo name with leading/trailing slashes
 export default defineConfig({
+  base: "/orcs/",
   server: { open: true },
-  build: { sourcemap: true }
+  build: { sourcemap: true, outDir: "dist" }
 });
