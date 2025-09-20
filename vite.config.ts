f6f1c3d244d9054f57530ea252c06f57a59014ab
testlearnernord <toleora@gmail.com>
2025-09-20 23:09:54 +0200

    chore(vite): enable @vitejs/plugin-react-swc and react-jsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'node:path';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   base: '/orcs/',
+  plugins: [react()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
